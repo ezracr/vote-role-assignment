de2d5c659f802981d23df32ccb3f4fd83b4244da
ezra <96570988+ezracr@users.noreply.github.com>
2021-12-27 13:59:44 +0200

    chore: remove unused permissions

--- src/client.ts
+++ src/client.ts
@@ -4,7 +4,7 @@ const client = new dsc.Client({
   intents: [
     dsc.Intents.FLAGS.GUILDS, dsc.Intents.FLAGS.GUILD_MESSAGES,
   ],
-  partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+  // partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
 })
 
 export default client

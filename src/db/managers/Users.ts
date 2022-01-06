d9818ac4db4e285643964b494657d51c9dd03558
ezra <96570988+ezracr@users.noreply.github.com>
2022-01-07 00:44:41 +0200

    bug: fix can't undo vote when it's not possible to remove the user from users

--- src/db/managers/Users.ts
+++ src/db/managers/Users.ts
@@ -14,14 +14,18 @@ class Users {
     return rows[0]
   }
 
-  async delById(data: Pick<User, 'id'>, client?: PoolClient): Promise<User['id'] | undefined> {
+  async delById(data: Pick<User, 'id'>, client?: PoolClient, savepoint?: string): Promise<User['id'] | undefined> {
     try {
       await (client ?? pool).query<User>(`
         DELETE FROM users us WHERE us."id" = $1
     `, [data.id])
 
       return data.id
-    } catch (e: unknown) { } // eslint-disable-line no-empty
+    } catch (e: unknown) {
+      if (client && savepoint) {
+        await client.query(`ROLLBACK TO ${savepoint}`)
+      }
+    }
   }
 }
 
